da7c0c657bf7985c72ad551ac441de8290c4a5ed
lauramesquitabruel <lauramesquitabruel@gmail.com>
2026-03-03 20:50:27 -0300

    formatando matriz com pandas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 S = "atagct"
 T = "ctaggcta"
@@ -6,11 +7,12 @@ T = "ctaggcta"
 n_s = len(S)
 n_t = len(T)
 
-matriz_substrings_comuns = np.zeros((n_s, n_t))
+matriz_substrings_comuns = np.zeros((n_s, n_t), dtype=int)
 
 for i in range (0, n_s):
     for j in range(0, n_t):
         if S[i] == T[j]:
             matriz_substrings_comuns[i][j] = 1
 
-print(matriz_substrings_comuns)
+df = pd.DataFrame(matriz_substrings_comuns, index=list(S), columns=list(T))
+print(df)
