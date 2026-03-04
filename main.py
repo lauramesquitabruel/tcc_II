ede7859e8f08216870c5ac3e0114d36afbeb73be
lauramesquitabruel <lauramesquitabruel@gmail.com>
2026-03-03 23:22:20 -0300

    grafos exclusivos

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aresta import Aresta
 
 S = "atagct"
 T = "ctaggcta"
-
+ALFABETO = ['a', 't', 'g', 'c']
 n_s = len(S)
 n_t = len(T)
 
@@ -19,14 +19,14 @@ for i in range (0, n_s):
         if S[i] == T[j]:
             matriz_substrings_comuns[i][j] = 1
 
-for i in range(len(S)):
-    for j in range(i, len(S)):
+for i in range(n_s):
+    for j in range(i, n_s):
         substring_s = S[i:j+1]
         if substring_s in T:
             grafo_s.insere_aresta(Aresta(i, j))
 
-for i in range(len(T)):
-    for j in range(i, len(T)):
+for i in range(n_t):
+    for j in range(i, n_t):
         substring_t = T[i:j+1]
         if substring_t in S:
             grafo_t.insere_aresta(Aresta(i, j))
@@ -34,6 +34,43 @@ for i in range(len(T)):
 df = pd.DataFrame(matriz_substrings_comuns, index=list(S), columns=list(T))
 print(df)
 
-print('\n')
-grafo_s.imprime()
-grafo_t.imprime()
+print('\n GRAFOS DE SUBSTRINGS COMUNS')
+grafo_s.imprime(S)
+grafo_t.imprime(T)
+
+a_s = []
+a_t = []
+
+for i in range (0, len(ALFABETO)):
+    occ_s = S.count(ALFABETO[i])
+    occ_t = T.count(ALFABETO[i])
+        
+    if occ_s > occ_t:
+        a_s.append(ALFABETO[i])
+    if occ_t > occ_s:
+        a_t.append(ALFABETO[i])
+
+print(f'ROTULOS ABUNDANTES \n T: {a_t} \n S: {a_s}')
+
+grafo_exclusivos_s = Grafo(n_s)
+grafo_exclusivos_t = Grafo(n_t)
+
+abudantes = set(a_s)
+for i in range(n_s):
+    for j in range(i, n_s):
+        substring_s = S[i:j+1]
+        validacao = set(substring_s)
+        if validacao.issubset(abudantes):
+            grafo_exclusivos_s.insere_aresta(Aresta(i, j))
+
+abudantes = set(a_t)
+for i in range(n_t):
+    for j in range(i, n_t):
+        substring_t = T[i:j+1]
+        validacao = set(substring_t)
+        if validacao.issubset(abudantes):
+            grafo_exclusivos_t.insere_aresta(Aresta(i, j))
+
+print('\n GRAFOS DE BLOCOS EXCLUSIVOS')
+grafo_exclusivos_s.imprime(S)
+grafo_exclusivos_t.imprime(T)
