3ccce09a0df4f91e4aec426b5f8ec901adae332d
lauramesquitabruel <lauramesquitabruel@gmail.com>
2026-05-01 15:15:40 -0300

    feat: formatação do tempo de saída

--- utils/saida.py
+++ utils/saida.py
@@ -76,7 +76,7 @@ def imprime_saida(S, T, log_file, t1, t2, t3):
                     log.write(f'Partições Selecionadas = ( {final_part_selecionadas} )\n')
 
                     log.write(f"Tempo de execução para criação das estruturas: {t2 - t1:.6f} segundos\n")
-                    log.write(f"Tempo de execução do modelo: {t2 - t1:.6f} segundos")
+                    log.write(f"Tempo de execução do modelo: {t3 - t1:.6f} segundos")
 
             except FileNotFoundError:
                 print(f"Error: The file '{file_path}' was not found.")
